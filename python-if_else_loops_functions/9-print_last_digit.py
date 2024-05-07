289cb3c4bf61eef55fc4783d8f6f5122a3914325
kieranhirsh <kieranhirsh@gmail.com>
2024-05-07 16:41:49 +1000

    Python - if/else, loops, functions. Task 9. There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    if number < 0:
+        last = abs(number % 10 - 10)
+    else:
+        last = number % 10
+    print(last, end="")
+    return last
