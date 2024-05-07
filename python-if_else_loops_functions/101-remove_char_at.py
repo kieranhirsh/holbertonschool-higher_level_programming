56e343386cdb211d4327bc41ddf17a1325ace50c
kieranhirsh <kieranhirsh@gmail.com>
2024-05-07 22:13:47 +1000

    Python - if/else, loops, functions. Task 14. Remove at position. fixing negative n case

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
-    return str[:n] + str[n + 1:]
+    if n >= 0:
+        return str[:n] + str[n + 1:]
+    elif n < 0:
+        return str
+    else:
+        return "n is neither positive nor negative"
