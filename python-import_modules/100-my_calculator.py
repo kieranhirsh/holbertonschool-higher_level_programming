d7f8ef0d2e82425dee8a96cc2de659dcff307095
kieranhirsh <kieranhirsh@gmail.com>
2024-05-08 11:43:55 +1000

    Python - import & modules. Task 6. Build my own calculator! going back to python3.8

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -11,15 +11,14 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    match sys.argv[2]:
-        case "+":
-            print("{} {} {} = {}".format(a, sys.argv[2], b, calc.add(a, b)))
-        case "-":
-            print("{} {} {} = {}".format(a, sys.argv[2], b, calc.sub(a, b)))
-        case "*":
-            print("{} {} {} = {}".format(a, sys.argv[2], b, calc.mul(a, b)))
-        case "/":
-            print("{} {} {} = {}".format(a, sys.argv[2], b, calc.div(a, b)))
-        case _:
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
+    if sys.argv[2] == "+":
+        print("{} {} {} = {}".format(a, sys.argv[2], b, calc.add(a, b)))
+    elif sys.argv[2] == "-":
+        print("{} {} {} = {}".format(a, sys.argv[2], b, calc.sub(a, b)))
+    elif sys.argv[2] == "*":
+        print("{} {} {} = {}".format(a, sys.argv[2], b, calc.mul(a, b)))
+    elif sys.argv[2] == "/":
+        print("{} {} {} = {}".format(a, sys.argv[2], b, calc.div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
