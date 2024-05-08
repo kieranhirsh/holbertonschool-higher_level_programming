8510f59e356c63e552a97b82c97662b24cd944d5
kieranhirsh <kieranhirsh@gmail.com>
2024-05-08 11:15:33 +1000

    Python - import & modules. Task 4. Who are you? making code cleaner

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,8 +3,6 @@
 if __name__ == "__main__":
     import hidden_4
 
-    to_print = dir(hidden_4)
-
-    for i in range(len(to_print)):
-        if to_print[i][0] != "_" or to_print[i][1] != "_":
-            print(to_print[i])
+    for name in dir(hidden_4):
+        if name[0] != "_" or i[name] != "_":
+            print(name)
