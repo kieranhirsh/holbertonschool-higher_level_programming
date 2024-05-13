43825f3cebd8c659cef4617f8a7433b11f6bd5f9
kieranhirsh <kieranhirsh@gmail.com>
2024-05-13 13:28:13 +1000

    Python - Data Structures: Lists, Tuples. Task 9. Find the max

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    max = 0
+    for ii in range(0,len(my_list)):
+        if my_list[ii] > max:
+            max = my_list[ii]
+
+    return max(my_list)
