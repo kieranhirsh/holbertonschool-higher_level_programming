7ed4e9b462bbc4d9bc6b4a46de66ad3559b753c7
kieranhirsh <kieranhirsh@gmail.com>
2024-05-13 14:47:15 +1000

    Python - More Data Structures: Set, Dictionary. Task 0. Squared simple. code style validation

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -6,5 +6,5 @@ def square_matrix_simple(matrix=[]):
         new_matrix[ii] = [None] * len(matrix[ii])
         for jj in range(len(new_matrix[ii])):
             new_matrix[ii][jj] = matrix[ii][jj] ** 2
-    
+
     return new_matrix
