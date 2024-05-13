f1dff3b7a2a5eba494cd49f7c1a8f3c7417ddbbd
kieranhirsh <kieranhirsh@gmail.com>
2024-05-13 13:30:44 +1000

    Python - Data Structures: Lists, Tuples. Task 9. Find the max. fixing empty list case

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return "None"
+    
     max = 0
     for ii in range(0,len(my_list)):
         if my_list[ii] > max:
