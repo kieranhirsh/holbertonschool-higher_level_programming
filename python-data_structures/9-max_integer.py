d5e8ecbd20cbd46e77096dbf3ed91ce109b5a75a
kieranhirsh <kieranhirsh@gmail.com>
2024-05-13 13:31:26 +1000

    Python - Data Structures: Lists, Tuples. Task 9. Find the max. fixing all negative case

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,8 +4,8 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return "None"
     
-    max = 0
-    for ii in range(0,len(my_list)):
+    max = my_list[0]
+    for ii in range(1,len(my_list)):
         if my_list[ii] > max:
             max = my_list[ii]
 
