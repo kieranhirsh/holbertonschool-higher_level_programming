cb69221810876e4499d01fdf04863f9b962ee02e
kieranhirsh <kieranhirsh@gmail.com>
2024-05-13 16:19:18 +1000

    Python - More Data Structures: Set, Dictionary. Task 6. Print sorted dictionary. code style validation

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    sort_dict = sorted(a_dictionary)
+    sort_dic = sorted(a_dictionary)
 
-    for ii in range(len(sort_dict)):
-        print("{0}: {1}".format(sort_dict[ii], a_dictionary.get(sort_dict[ii])))
+    for ii in range(len(sort_dic)):
+        print("{0}: {1}".format(sort_dic[ii], a_dictionary.get(sort_dic[ii])))
