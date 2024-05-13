ad677aba6bd7348dbe02e0c9c3ddae42b5a18ac9
kieranhirsh <kieranhirsh@gmail.com>
2024-05-13 11:38:57 +1000

    Python - Data Structures: Lists, Tuples. Task 6. Lists of lists = Matrix. trying to fix empty matrix case

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,12 @@
 
 def print_matrix_integer(matrix=[[]]):
     for ii in range(len(matrix)):
+        ending = " "
         for jj in range(len(matrix[ii])):
-            if jj == len(matrix[ii]) - 1:
-                print("{:d}".format(matrix[ii][jj]))
-            else:
-                print("{:d} ".format(matrix[ii][jj]), end="")
+            if jj == len(matrix[ii]) - 1 and ii != len(matrix) - 1:
+                ending = "\n"
+            elif jj == len(matrix[ii]) - 1 and ii == len(matrix) - 1:
+                ending = ""
+
+            print("{:d}".format(matrix[ii][jj]), end=ending)
+    print("")
