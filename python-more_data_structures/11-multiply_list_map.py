66466bb4fd2886c8a595db13d2e6b858fc0e0caa
kieranhirsh <kieranhirsh@gmail.com>
2024-05-14 16:29:37 +1000

    Python - More Data Structures: Set, Dictionary. Task 11. Multiply by using map. making code 3 lines long

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-
 def multiply_list_map(my_list=[], number=0):
-    def mult(n):
-        return n * number
-
-    return list(map(mult, my_list))
+    return list(map(lambda n : n * number, my_list))
