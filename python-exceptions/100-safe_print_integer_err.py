4d75922d9402cb10b7790dd8aa550d8459df7b0a
kieranhirsh <kieranhirsh@gmail.com>
2024-05-14 23:25:24 +1000

    Python - Exceptions. Task 7. Safe integer print with error message. handling TypeError

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,8 @@ def safe_print_integer_err(value):
     except ValueError:
         print("Exception: Unknown format code 'd' for object of type 'str'", file=sys.stderr)
         return False
+    except TypeError:
+        print("TypeError: unsupported format string passed to set.__format__", file=sys.stderr)
+        return False
     else:
         return True
