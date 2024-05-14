30d354136e7de957515168ed0908a5f818935447
kieranhirsh <kieranhirsh@gmail.com>
2024-05-14 22:14:14 +1000

    Python - Exceptions. Task 3. Integers division with debug

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    c = 0
+
+    try:
+        c = a / b
+    except ZeroDivisionError:
+        pass
+    finally:
+        print("Inside result: {}".format(c))
+
+    return c
