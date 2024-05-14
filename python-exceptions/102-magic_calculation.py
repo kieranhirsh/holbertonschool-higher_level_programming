5dcd0e9463f12852a76fada83c6fac8850c0542c
kieranhirsh <kieranhirsh@gmail.com>
2024-05-14 23:58:44 +1000

    Python - Exceptions. Task 9. ByteCode -> Python #4. changing exception condition

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -5,7 +5,7 @@ def magic_calculation(a, b):
 
     for i in range(1, 3):
         try:
-            if i > a:
+            if i >= a:
                 raise Exception('Too far')
             else:
                 result += (a ** b) / i
