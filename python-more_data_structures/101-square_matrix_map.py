c5a3cabf165fda8490409ef55914731c15751780
kieranhirsh <kieranhirsh@gmail.com>
2024-05-14 17:36:46 +1000

    Python - More Data Structures: Set, Dictionary. Task 14. Squared by using map. not allowed to use for

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda vect: [n ** 2 for n in vect], matrix))
+    return list(map(lambda vect: list(map(lambda n: n ** 2, vect)), matrix))
