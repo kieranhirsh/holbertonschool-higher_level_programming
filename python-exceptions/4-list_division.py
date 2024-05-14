ef42b9d77c038b1d2dd51f0011802d436a63ca5b
kieranhirsh <kieranhirsh@gmail.com>
2024-05-14 22:44:42 +1000

    Python - Exceptions. Task 4. Divide a list. have to use finally:

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 
 def list_division(my_list_1, my_list_2, list_length):
-    div_list = [None] * list_length
+    div_list = []
+
     for ii in range(list_length):
         try:
-            div_list[ii] = my_list_1[ii] / my_list_2[ii]
+            result = my_list_1[ii] / my_list_2[ii]
         except IndexError:
             print("out of range")
-            div_list[ii] = 0
+            result = 0
         except TypeError:
             print("wrong type")
-            div_list[ii] = 0
+            result = 0
         except ZeroDivisionError:
             print("division by 0")
-            div_list[ii] = 0
+            result = 0
+        finally:
+            div_list.append(result)
 
     return div_list
