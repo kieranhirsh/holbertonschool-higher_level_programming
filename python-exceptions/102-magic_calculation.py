ae3f032d1201724d59fe3d7f682dc170d8d8d6b4
kieranhirsh <kieranhirsh@gmail.com>
2024-05-14 23:48:55 +1000

    Python - Exceptions. Task 9. ByteCode -> Python #4. forgot to define my function

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
-result = 0
+def magic_calculation(a, b):
+    result = 0
 
-for i in range(1, 4):
-    try:
-        if i > a:
-            raise Exception('Too far')
-        else:
-            result += (b ** a) / i
-    except Exception:
-        result += a + b
+    for i in range(1, 4):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += (b ** a) / i
+        except Exception:
+            result += a + b
