308adac719eb75fcd39d96aa86e65e05b74ce5a2
kieranhirsh <kieranhirsh@gmail.com>
2024-05-14 22:05:41 +1000

    Python - Exceptions. Task 2. Print and count integers

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+
+    for ii in range(x):
+        try:
+            print("{:d}".format(my_list[ii]), end="")
+        except (ValueError, TypeError):
+            pass
+        else:
+            count += 1
+
+    print("")
+
+    return count
