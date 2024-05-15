e577d344b8b31c2fc60a9cddc68ca33b5626c4a5
kieranhirsh <kieranhirsh@gmail.com>
2024-05-15 11:45:32 +1000

    Python - Classes and Objects. Task 0. My first square. the checker didn't like my documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# module documentation. is empty for now
+''' module documentation. is empty for now '''
 
 class Square:
-    # this class will define a square, it does nothing for now though
+    ''' this class will define a square, it does nothing for now though '''
     pass
