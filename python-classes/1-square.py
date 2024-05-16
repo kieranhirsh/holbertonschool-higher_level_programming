41e398b0751999553e31b295e866e31b804fd011
kieranhirsh <kieranhirsh@gmail.com>
2024-05-16 10:33:56 +1000

    Python - Classes and Objects. Task 1. Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' module documentation. is empty for now '''
+
+
+class Square:
+    ''' this class defines a square '''
+    def __init__(self, size):
+        """
+        Initialize a square
+
+        Args:
+            size (int): size of the  square
+        """
+        self.__size = size
