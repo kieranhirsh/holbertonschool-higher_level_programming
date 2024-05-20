208b26dfd001b55d5d7eed5c3d4e2ee8956efac9
kieranhirsh <kieranhirsh@gmail.com>
2024-05-20 15:18:31 +1000

    Python - Test-driven development. 5. Max integer - Unittest. TIL tests need to have their names start with test_

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,25 +7,25 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
-    def max_at_end(self):
+    def test_max_at_end(self):
         self.assertEqual(max_integer([1, 2, 3]), 3)
 
-    def max_at_middle(self):
+    def test_max_at_middle(self):
         self.assertEqual(max_integer([1, 3, 2]), 3)
 
-    def max_at_start(self):
+    def test_max_at_start(self):
         self.assertEqual(max_integer([3, 2, 1]), 3)
 
-    def one_negative(self):
+    def test_one_negative(self):
         self.assertEqual(max_integer([1, 2, -3]), 2)
 
-    def only_negative(self):
+    def test_only_negative(self):
         self.assertEqual(max_integer([-1, -2, -3]), -1)
 
-    def one_element(self):
+    def test_one_element(self):
         self.assertEqual(max_integer([1]), 1)
 
-    def empty_list(self):
+    def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
 
 
