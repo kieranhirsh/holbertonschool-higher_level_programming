bc27cd3c8b417fcc49a7b0c2f7beb6862870ae0d
kieranhirsh <kieranhirsh@gmail.com>
2024-05-20 14:03:07 +1000

    Python - Test-driven development. 4. Text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''' say_my_name module '''
+
+
+def text_indentation(text):
+    ''' this function prints a text with two lines after certain characters
+
+    Args:
+        size (int): a size of the square
+    '''
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    newline_chars = [".", "?", ":"]
+
+    char = 0
+    while char < len(text):
+        print(text[char], end="")
+        if text[char] in newline_chars:
+            print("\n")
+            char += 1
+        char += 1
