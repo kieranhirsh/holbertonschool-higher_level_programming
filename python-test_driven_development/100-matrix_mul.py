5830aff0afd5e95899c7a2bc009eba81703b231e
kieranhirsh <kieranhirsh@gmail.com>
2024-05-20 16:37:04 +1000

    Python - Test-driven development. 6. Matrix multiplication. no tests yet

---
+++ python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+''' matrix_mul module '''
+
+
+def matrix_mul(m_a, m_b):
+    ''' this function multiplies two matrices together
+
+    Args:
+        m_a (list of list of ints/floats): a matrix
+        m_b (list of list of ints/floats): b matrix
+
+    Returns:
+        (list of list of ints/floats): a new matrix, equal to m_a * m_b
+    '''
+    if ((len(m_a) > 2 and not all(len(row) == 1 for row in m_a)) or
+            (len(m_b) > 2 and not all(len(row) == 1 for row in m_b)) or
+            (any(len(row) > 2 for row in m_a) and len(m_a) != 1) or
+            (any(len(row) > 2 for row in m_b) and len(m_b) != 1)):
+        raise NotImplementedError("I'm too lazy to multiply matrices larger "
+                                  "than 2x2")
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+    if m_a == [] or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+    if m_b == [] or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
+    if not all(len(row) == len(m_a[0]) for row in m_a):
+        raise TypeError("each row of m_a must be of the same size")
+    if not all(len(row) == len(m_b[0]) for row in m_b):
+        raise TypeError("each row of m_b must be of the same size")
+
+    new_matrix = []
+    for ii in range(len(m_a)):
+        new_matrix.append([])
+        for jj in range(len(m_b[0])):
+            new_matrix[ii].append(0)
+            for kk in range(len(m_a[0])):
+                if (not isinstance(m_a[ii][jj], int) and
+                        not isinstance(m_a[ii][jj], float)):
+                    raise TypeError("m_a should contain only integers or "
+                                    "floats")
+                if (not isinstance(m_b[ii][jj], int) and
+                        not isinstance(m_b[ii][jj], float)):
+                    raise TypeError("m_b should contain only integers or "
+                                    "floats")
+
+                new_matrix[ii][jj] += m_a[ii][kk] * m_b[kk][jj]
+
+    return new_matrix
