6a812559e7761ddb25df651c2fbc77c0539879d2
kieranhirsh <kieranhirsh@gmail.com>
2024-05-20 15:14:38 +1000

    Python - Test-driven development. 5. Max integer - Unittest. trying to bug fix

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
-class MaxIntegerTests(unittest.TestCase):
+class TestMaxInteger(unittest.TestCase):
 
     def max_at_end(self):
         self.assertEqual(max_integer([1, 2, 3]), 3)
