af170a265b01a622503a2f2d34de37f0d6a100a0
kieranhirsh <kieranhirsh@gmail.com>
2024-05-23 15:17:18 +1000

    Python - Inheritance. Task 4. Only sub class of. code style validation

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@
 def inherits_from(obj, a_class):
     '''
     function that checks if obj is an instance of a class that inherited
-    (directly or indirectly) from the specified class 
+    (directly or indirectly) from the specified class
 
     Returns:
         True, if true
