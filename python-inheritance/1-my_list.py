5f9497497cd67f2851e4d9cdc7eadffbb43d7555
kieranhirsh <kieranhirsh@gmail.com>
2024-05-23 14:58:32 +1000

    Python - Inheritance. Task 1. My list. code style validation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,7 +5,7 @@
 class MyList(list):
     ''' this class is MyList '''
     def print_sorted(self):
-        """ 
+        """
         Method to print a list in ascending order
         """
         print_list = self.copy()
