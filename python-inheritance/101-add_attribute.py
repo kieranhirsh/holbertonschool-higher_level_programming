9b47ce51e929b9e9691ceff6212ff26e6d736bf2
kieranhirsh <kieranhirsh@gmail.com>
2024-05-23 17:36:19 +1000

    Python - Inheritance. Task 13. Can I?

---
+++ python-inheritance/101-add_attribute.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add_attribute(obj, name, value):
+    """
+    This function adds a new attribute to an object if possible
+
+    Args:
+        obj: the object
+        name: the attribute name
+        value: the attribute value
+    """
+    if hasattr(obj, "__dict__"):
+        setattr(obj, name, value)
+    else:
+        raise TypeError("can't add new attribute")
