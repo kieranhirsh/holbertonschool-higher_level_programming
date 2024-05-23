24271447d661df0a35ceaa781102e853a5d518d9
kieranhirsh <kieranhirsh@gmail.com>
2024-05-23 14:26:37 +1000

    Python - Inheritance. Task 1. My list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" a module """
+
+
+class MyList(list):
+    ''' this class is MyList '''
+    def print_sorted(self):
+        """ 
+        Method to print a list in ascending order
+        """
+        print_list = self.copy()
+        print_list.sort()
+        print(print_list)
