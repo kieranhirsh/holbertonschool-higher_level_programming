63fac0026255483770ce40f3ffd54609ab674af2
kieranhirsh <kieranhirsh@gmail.com>
2024-05-25 01:00:37 +1000

    Python - Abstract Classes and Interfaces. Task 0. Abstract Animal Class and its Subclasses

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+''' module documentation '''
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    ''' this class defines an Animal '''
+    @abstractmethod
+    def sound(self):
+        """
+        Abstract method define the sound associated with an Animal
+        """
+        pass
+
+class Dog(Animal):
+    ''' this class defines a Dog, it inherits from Animal '''
+    def sound(self):
+        """
+        Method define the sound associated with a dog
+        """
+        return "bark"
+
+class Cat(Animal):
+    ''' this class defines a Cat, it inherits from Animal '''
+    def sound(self):
+        """
+        Method define the sound associated with a cat
+        """
+        return "meow"
