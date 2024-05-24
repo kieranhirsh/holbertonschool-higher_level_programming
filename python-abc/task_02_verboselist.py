bbe8f55fde691eebec505a26fa8b8fd30caff12e
kieranhirsh <kieranhirsh@gmail.com>
2024-05-25 02:28:35 +1000

    Python - Abstract Classes and Interfaces. Task 2. Extending the Python List with Notifications

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+''' module documentation '''
+
+
+class VerboseList(list):
+    ''' this class defines a verbose list, it inherits from the in-built python
+    list '''
+    def append(self, item):
+        """
+        method to append an item to the list
+
+        Args:
+            item: the item to append
+        """
+        super().append(item)
+        print("Added [{}] to the list.".format(item))
+
+    def extend(self, items):
+        """
+        method to extend a list
+
+        Args:
+            items (list): the items to append to the list
+        """
+        super().extend(items)
+        print("Extended the list with [{}] items.".format(len(items)))
+
+    def remove(self, item):
+        """
+        method to remove an item from the list
+
+        Args:
+            item: the item to remove
+        """
+        super().remove(item)
+        print("Removed [{}] from the list.".format(item))
+
+    def pop(self, idx=None):
+        """
+        method to pop an item from the list
+
+        Args:
+            idx (int): the index to pop
+        """
+        if idx is None:
+            super().pop(len(self) - 1)
+            print("Removed [{}] from the list.".format(len(self)))
+        else:
+            super().pop(idx)
+            print("Removed [{}] from the list.".format(idx))
+
