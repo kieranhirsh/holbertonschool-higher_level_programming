72c53241f8a1e56c5531f7a52f5e693855750c72
kieranhirsh <kieranhirsh@gmail.com>
2024-05-26 23:19:36 +1000

    Python - Abstract Classes and Interfaces. Task 3. CountedIterator - Keeping Track of Iteration. trying to fix exception handling

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -4,17 +4,20 @@
 
 class CountedIterator():
     ''' a class '''
-    def __init__(self, iterator):
+    def __init__(self, some_iterable):
         ''' initialisation method '''
-        self.iterator = iter(iterator)
+        self.iterator = iter(some_iterable)
         self.counter = 0
+
+    def __iter__(self):
+        ''' iter method '''
+        return self
 
     def __next__(self):
         ''' next method '''
         self.counter += 1
         try:
-            item = next(self.iterator)
-            return item
+            return next(self.iterator)
         except StopIteration:
             raise StopIteration
 
