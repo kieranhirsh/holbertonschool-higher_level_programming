b031b63e640f549bade2c901cdeff879e18b1e93
kieranhirsh <kieranhirsh@gmail.com>
2024-05-26 18:47:07 +1000

    Python - Abstract Classes and Interfaces. Task 4. The Enigmatic FlyingFish - Exploring Multiple Inheritance

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+''' module documentation '''
+
+
+class Fish():
+    ''' this class defines a Fish '''
+    def swim(self):
+        """
+        Method
+        """
+        print("The fish is swimming")
+
+    def habitat(self):
+        """
+        Method
+        """
+        print("The fish lives in water")
+
+class Bird():
+    ''' this class defines a Bird '''
+    def fly(self):
+        """
+        Method
+        """
+        print("The fish is flying")
+
+    def habitat(self):
+        """
+        Method
+        """
+        print("The fish lives in water")
+
+class FlyingFish():
+    ''' this class defines a FlyingFish '''
+    def fly(self):
+        """
+        Method
+        """
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        """
+        Method
+        """
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        """
+        Method
+        """
+        print("The flying fish lives both in water and the sky!")
