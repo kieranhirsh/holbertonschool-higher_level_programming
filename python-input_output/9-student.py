cfa5eca12008d9562cb3f9be2ebc61afe75f4970
kieranhirsh <kieranhirsh@gmail.com>
2024-05-29 00:29:33 +1000

    Python - Input/Output. Task 9. Student to JSON

---
+++ python-input_output/9-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+''' module documentation '''
+
+
+class Student():
+    """
+    Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        '''
+        initialisation method
+
+        Arguments:
+        first_name (str): student's first name
+        last_name (str): student's last name
+        age (int): student's age
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''
+        This module returns the JSON representation of the class
+
+        Returns:
+        JSON representation of the object
+        '''
+        return (self.__dict__)
