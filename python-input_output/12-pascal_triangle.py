c1e1bdbf460166030288abb9e548e10df7344856
kieranhirsh <kieranhirsh@gmail.com>
2024-05-29 02:18:35 +1000

    Python - Input/Output. Task 12. Pascal's Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' module documentation '''
+
+
+def pascal_triangle(n):
+    '''
+    this function returns Pascals triangle
+
+    Arguments:
+        n (int): the desired Pascal's triangle size
+            
+    Returns:
+        triangle (list of lists of int): Pascal's triangle of size n
+    '''
+    triangle = []
+
+    for ii in range(n):
+        triangle.append([None] * (ii + 1))
+        triangle[ii][0] = 1
+        triangle[ii][-1] = 1
+        for jj in range(1, ii):
+            triangle[ii][jj] = (triangle[ii - 1][jj - 1] + triangle[ii - 1][jj])
+
+    return triangle
