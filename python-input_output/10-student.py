65211bdd5b179d1c73e0c59f05370e0d5f78cd21
kieranhirsh <kieranhirsh@gmail.com>
2024-05-29 01:00:15 +1000

    Python - Input/Output. Task 10. Student to JSON with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+''' module documentation '''
+
+
+class Student():
+    """
+    Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        '''
+        initialisation method
+
+        Arguments:
+        first_name (str): student's first name
+        last_name (str): student's last name
+        age (int): student's age
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''
+        This module returns the JSON representation of the class
+
+        Arguments:
+        attrs (list of str): the atrributes to return (return all if None)
+
+        Returns:
+        JSON representation of the object
+        '''
+        if attrs is None:
+            return self.__dict__
+
+        temp = self.__dict__
+        Jason = dict()
+
+        for key in temp:
+            if key in attrs:
+                Jason[key] = temp[key]
+
+        return Jason
