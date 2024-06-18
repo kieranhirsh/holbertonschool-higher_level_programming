7356fdaa86c3a42c9bc6d4fb0cd827f71893ff47
kieranhirsh <kieranhirsh@gmail.com>
2024-06-18 14:44:19 +1000

    SQL - More queries. Task 4. ID can't be null

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
--- Task 4: Write a script that creates a table called first_table in the current database in your MySQL server.
+-- Task 3: Write a script that creates the table force_name on your MySQL server.
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
