c0b9f19ef39d8c935ef3982840f7bc64b07b0e19
kieranhirsh <kieranhirsh@gmail.com>
2024-06-20 11:18:34 +1000

    SQL - More queries. Task 11. Genre ID for all shows

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
--- Task 9: Write a script that lists all cities contained in the database hbtn_0d_usa.
+-- Task 10: Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
 LEFT JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
