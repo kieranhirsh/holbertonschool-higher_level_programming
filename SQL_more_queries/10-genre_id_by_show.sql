fcdb3deeefaf4c228d78aa2b1310efd9fc1648f1
kieranhirsh <kieranhirsh@gmail.com>
2024-06-20 11:27:09 +1000

    SQL - More queries. Task 12. No genre

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
 -- Task 10: Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
-LEFT JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+LEFT JOIN tv_shows
+ON tv_shows.id=tv_show_genres.show_id
 ORDER BY title ASC, genre_id ASC;
--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,7 @@
--- Task 11: Write a script that lists all shows contained in the database hbtn_0d_tvshows.
+-- Task 12: Write a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
 ORDER BY title ASC, genre_id ASC;
