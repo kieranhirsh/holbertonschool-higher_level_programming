2393b9a39c227d6c7883a1416826ed23dd24d39e
kieranhirsh <kieranhirsh@gmail.com>
2024-06-22 18:15:04 +1000

    Python - Object-relational mapping. Task 3. SQL Injection...

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" the checker demands documentation """
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    if ";" in sys.argv[4]:
+        sys.exit()
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
+
+    cr = db.cursor()
+    cr.execute("SELECT *\
+               FROM states\
+               WHERE BINARY name = '{}'\
+               ORDER BY id ASC;".format(sys.argv[4]))
+    rows = cr.fetchall()
+    for row in rows:
+        print(row)
+
+    cr.close()
+    db.close()
