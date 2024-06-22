d06c74aa4825393aa6837b4a1d8b8fa981fefe8f
kieranhirsh <kieranhirsh@gmail.com>
2024-06-23 03:05:36 +1000

    Python - Object-relational mapping. Task 6. First state model. code style validation

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,10 +7,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """
     The state class
     """
     __tablename__ = 'states'
     id = Column(Integer, autoincrement=True, primary_key=True)
-    name =  Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
