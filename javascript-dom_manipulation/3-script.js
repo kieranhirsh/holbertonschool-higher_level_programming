a0869e2b16d5dbfc03269cbe348e07ebc86b40a6
kieranhirsh <kieranhirsh@gmail.com>
2024-07-16 12:08:23 +1000

    JavaScript DOM manipulation. Task 3. Toggle classes. optimisations

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,9 +1,6 @@
 document.getElementById("toggle_header").onclick = toggleClass;
 
 function toggleClass() {
-  if (document.querySelector('header').classList.contains("red")) {
-    document.querySelector('header').classList.replace("red", "green")
-  } else if (document.querySelector('header').classList.contains("green")) {
-    document.querySelector('header').classList.replace("green", "red")
-  }
+  document.querySelector('header').classList.toggle("red")
+  document.querySelector('header').classList.toggle("green")
 }
